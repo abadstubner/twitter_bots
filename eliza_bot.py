d3f06a4741a3e86778002c058075f0dfd09a4f3b
Andrew Badstubner <andrew@badstubner.com>
2014-02-19 17:58:08 -0500

    Working demo, including consideration for twitter rate limits.

--- eliza_bot.py
+++ eliza_bot.py
@@ -3,25 +3,48 @@
 
 from utils import *
 from datetime import datetime
+from time import sleep
 from eliza import eliza
+from twitter.api import TwitterHTTPError
 
 t = get_twitter()
 therapist = eliza()
 
+print "** The Eliza Twitter Bot is Now Listening **"
+i=0
+
 while True:
+    # Run the search and get the results
+    i = i+1
+    print "Running query #%d" % i
     results = t.search.tweets(q="@drdaniellms", result_type="recent", since_id=get_max_id())
     save_max_id(results['search_metadata']['max_id_str'])
+    
+    # Loop through the results
     for result in results['statuses']:
         sender = result['user']['screen_name']
         their_tweet = result['text']
-
+        tweet_id = result['id_str']
+        
+        # Get the exact question
         question = their_tweet.replace("@" + sender, "")
         print "@" + sender + " asked: " + question
         
+        # Build the response
         reply = therapist.respond(question)
         twitter_response = "@" + sender + " " + reply
         twitter_response = twitter_response.replace("@drdaniellms", "")
         
-        t.statuses.update(status=twitter_response)
-        print "----- " + twitter_response
+        # Post the response to twitter
+        try:
+            t.statuses.update(status=twitter_response, in_reply_to_status_id=tweet_id)
+            print "----- " + twitter_response
+        except TwitterHTTPError as t_e:
+            # it is common to get a duplicate status error
+            print t_e
+
+    # Sleep for 10 seconds to avoid pounding the Twitter API
+    # _and_ avoids rate limits
+    sleep(10)
+
 
--- simple_bot.py
+++ simple_bot.py
@@ -3,18 +3,33 @@
 
 from utils import *
 from datetime import datetime
+from time import sleep
 
 t = get_twitter()
 
+print "** The Eliza Twitter Bot is Now Listening **"
 
 while True:
+    # Run the search and get the results
     results = t.search.tweets(q="@drdaniellms", result_type="recent", since_id=get_max_id())
     save_max_id(results['search_metadata']['max_id_str'])
+
+    # Loop through the results
     for result in results['statuses']:
+        # Get the user name
         sender = result['user']['screen_name']
-        their_tweet = result['text']
+
+        # Build the response and post it
+        # Adding the current time to avoid duplicates
+        # It _is_ a hack, but it works for the demo
         current_time = str(datetime.now())
         t.statuses.update(status="Good morning @" + sender + ". The current time is " + current_time)
+        
+        # Log the action so we can follow along
         print "Replied to " + sender
+
+    # Sleep for 10 seconds to avoid pounding the Twitter API
+    # _and_ avoids rate limits
+    sleep(10)
 
 
