73e5c4f501bafdcede168510b40f28be9b77e660
Andrew Badstubner <andrew@badstubner.com>
2014-02-19 08:40:32 -0500

    Merged from origin.

--- eliza_bot.py
+++ eliza_bot.py
@@ -3,19 +3,25 @@
 
 from utils import *
 from datetime import datetime
+from eliza import eliza
 
 t = get_twitter()
+therapist = eliza()
 
-
-# This seems a little harsh on the pounding the API
 while True:
     results = t.search.tweets(q="@drdaniellms", result_type="recent", since_id=get_max_id())
     save_max_id(results['search_metadata']['max_id_str'])
     for result in results['statuses']:
         sender = result['user']['screen_name']
         their_tweet = result['text']
-        current_time = str(datetime.now())
-        t.statuses.update(status="Good morning @" + sender + ". The current time is " + current_time)
-        print "Replied to " + sender
 
+        question = their_tweet.replace("@" + sender, "")
+        print "@" + sender + " asked: " + question
+        
+        reply = therapist.respond(question)
+        twitter_response = "@" + sender + " " + reply
+        twitter_response = twitter_response.replace("@drdaniellms", "")
+        
+        t.statuses.update(status=twitter_response)
+        print "----- " + twitter_response
 
--- simple_bot.py
+++ simple_bot.py
@@ -2,18 +2,19 @@
 
 
 from utils import *
+from datetime import datetime
 
 t = get_twitter()
 
 
-# This seems a little harsh on the pounding the API
 while True:
     results = t.search.tweets(q="@drdaniellms", result_type="recent", since_id=get_max_id())
     save_max_id(results['search_metadata']['max_id_str'])
     for result in results['statuses']:
         sender = result['user']['screen_name']
         their_tweet = result['text']
-        t.statuses.update(status="Good morning @" + sender)
+        current_time = str(datetime.now())
+        t.statuses.update(status="Good morning @" + sender + ". The current time is " + current_time)
         print "Replied to " + sender
 
 
